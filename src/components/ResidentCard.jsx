98c2f9d023b04c45ba9d80158d88a1578f030017
OnilBlanco26 <onilblanco26@gmail.com>
2022-12-02 23:11:28 -0300

    Realizando algunos cambios en el diseño y optimizando el codigo

--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -12,39 +12,20 @@ const ResidentCard = ({url}) => {
         .catch(err => console.log(err))
     }, [])
 
-    const arrColor = ['gray' , 'green', 'red'];
-
-    const getColor = (array) => {
-        let indexColor;
-        if(resident?.status === 'Alive') {
-            indexColor = array[1];
-        } else if(resident?.status === 'Dead') {
-            indexColor = array[2];
-        } else if(resident?.status === 'unknown'){
-            indexColor = array[0];
-        }
-
-        return indexColor;
-    }
-
   
-
-    const objStyle = {
-        backgroundColor: getColor(arrColor)
-    }
-
 
   return (
     <article className='card'>
         <header className='card__header'>
             <img className='card__image' src={resident?.image} alt="" />
             <div className='card__status'>
-                <span className="circle" style={objStyle}><div></div></span>
+                <div className={`circle ${resident?.status}`}></div>
                 <span className='status__info'>{resident?.status}</span>
             </div>
         </header>
         <section className='card__section'>
             <h3 className='card__section-name'>{resident?.name}</h3>
+            <div className='card__line'></div>
             <ul className='card__section-list'>
                 <li className='card__section-list--item'><span className='card__section-list--span1'>Species</span><span className='card__section-list--span2'>{resident?.species}</span></li>
                 <li className='card__section-list--item'><span className='card__section-list--span1'>Origin</span><span className='card__section-list--span2'>{resident?.origin.name}</span></li>
