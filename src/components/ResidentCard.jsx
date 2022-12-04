55460c7f8be199d3615105cf1c0a8d52517ff8ff
OnilBlanco26 <onilblanco26@gmail.com>
2022-12-04 19:43:40 -0400

    Realizando multiiples cambios de diseño

--- src/components/ResidentCard.jsx
+++ src/components/ResidentCard.jsx
@@ -1,5 +1,9 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import imgrickAlive from '../assets/img/rickAlive.png'
+import imgrickDead from '../assets/img/rickDead.png'
+import imgrickUnk from '../assets/img/rickUnknown.png'
+
 
 const ResidentCard = ({url}) => {
 
@@ -12,11 +16,38 @@ const ResidentCard = ({url}) => {
         .catch(err => console.log(err))
     }, [])
 
+    const rickImgg =  () => {
+        let text = '';
+        if(resident?.status === 'Alive') {
+            text = imgrickAlive
+        } else if(resident?.status === 'Dead') {
+            text = imgrickDead
+        } else if(resident?.status === 'unknown'){
+            text = imgrickUnk
+        }
+        return text;
+    }
+
+    const fnRick = () => {
+        let text = '';
+        if(resident?.status === 'Alive') {
+            text = 'rickImg rickImgAlive'
+        } else if(resident?.status === 'Dead') {
+            text = 'rickImg'
+        } else if(resident?.status === 'unknown'){
+            text = 'rickImg'
+        }
+        return text;
+    }
   
 
   return (
     <article className='card'>
+        
         <header className='card__header'>
+            <div className='rickImg__card'>
+                <img className={fnRick()} src={rickImgg()} alt="" />
+            </div>
             <img className='card__image' src={resident?.image} alt="" />
             <div className='card__status'>
                 <div className={`circle ${resident?.status}`}></div>
@@ -24,8 +55,9 @@ const ResidentCard = ({url}) => {
             </div>
         </header>
         <section className='card__section'>
+           
             <h3 className='card__section-name'>{resident?.name}</h3>
-            <div className='card__line'></div>
+            <div className={`card__line ${resident?.status}`}></div>
             <ul className='card__section-list'>
                 <li className='card__section-list--item'><span className='card__section-list--span1'>Species</span><span className={resident?.status === 'Alive' || resident?.status === 'unknown' ? `card__section-list--span2 list__Alive` : `card__section-list--span2 list__Dead`}>{resident?.species}</span></li>
                 <li className='card__section-list--item'><span className='card__section-list--span1'>Origin</span><span className={resident?.status === 'Alive' || resident?.status === 'unknown' ? `card__section-list--span2 list__Alive` : `card__section-list--span2 list__Dead`}>{resident?.origin.name}</span></li>
